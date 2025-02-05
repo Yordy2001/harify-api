1eaee85e43ce0d6b6a79eb4eea45b1b525539878
Yordy2001 <kingyordi2001@gmail.com>
2025-02-05 11:35:04 -0400

    first commit, add docker compose, add database conn

---
+++ dockerfile
@@ -0,0 +1,23 @@
+# Usa una imagen base de Node.js
+FROM node:22
+
+# Establece el directorio de trabajo
+WORKDIR /app
+
+# Copia los archivos de configuración
+COPY package*.json ./
+
+# Instala las dependencias
+RUN yarn install
+
+# Copia el código fuente
+COPY . .
+
+# Compila la aplicación
+RUN yarn run build
+
+# Expone el puerto 3000
+EXPOSE 3000
+
+# Comando para ejecutar la aplicación
+CMD ["yarn", "run", "start:prod"]
---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +(process.env.DATABASE_PORT || 5432),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Solo para desarrollo
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
