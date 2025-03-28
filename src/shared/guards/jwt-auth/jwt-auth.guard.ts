ae9ef60b33e1e052f6e1e4e7a26bf31b4a2bc4bd
Yordy2001 <kingyordi2001@gmail.com>
2025-03-27 23:23:17 -0400

    feat add payload interface, add userId, subdomain, role in req

--- src/shared/guards/jwt-auth/jwt-auth.guard.ts
+++ src/shared/guards/jwt-auth/jwt-auth.guard.ts
@@ -7,16 +7,17 @@ export class JwtAuthGuard implements CanActivate {
 
   constructor(
     private jwtService: JwtService
-  ){}
+  ) { }
 
-  canActivate( context: ExecutionContext): boolean {
+  canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest<Request>();
     const token = request.cookies?.token;
-    
-    if(!token) throw new UnauthorizedException('No estás autenticado')
+
+    if (!token) throw new UnauthorizedException('No estás autenticado')
 
     try {
       const payload = this.jwtService.verify(token);
+
       request.user = payload;
       return true
     } catch (error) {
